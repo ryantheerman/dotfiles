5d3d2448f8e7124663db5b1956500227bc79921e
ryan theerman <ryantheerman@proton.me>
2025-04-02 00:21:59 -0400

    created ganymede dedicated graphics screenlayout scripts... needs work

---
+++ .screenlayout/ganymede-docked-dedicated.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output eDP-1-1 --mode 2560x1600 --pos 2528x2160 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-3-5 --off --output DP-1-3-6 --off --output DP-1-3-6-8 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-3-6-1 --off --output DP-1-3-6-1-8 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-1-3-6-1-1 --off
