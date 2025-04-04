d4c5788bf709ff5ed9f8a7457768d325e23fa0c4
ryan theerman <ryantheerman@proton.me>
2025-04-03 20:12:46 -0400

    renamed discrete gpu screenlayout scripts

---
+++ .screenlayout/ganymede-docked-discrete.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1200 --pos 1541x1440 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-3-5 --off --output DP-1-3-6 --off --output DP-1-3-6-8 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3-6-1 --off --output DP-1-3-6-1-8 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-3-6-1-1 --off
